98e2db93f18d28a69144b57fb18d95721b7ccf88
Mohaimenul <mahadihasan1603@gmail.com>
2021-10-12 06:31:59 +0600

    Initial Commit

---
+++ APWT_Lab_Task_Migration/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<h2>
+    <br>
+    Get Whatever You Want
+</h2>
+@endsection
---
+++ APWT_Lab_Task_Migration/resources/views/inc/topnav.blade.php
@@ -0,0 +1,10 @@
+<a class="btn btn-danger" href="{{route('home')}}"> Home </a>
+
+<a class="btn btn-primary" href="{{route('addProduct')}}"> Add Product </a>
+
+<a class="btn btn-primary" href="{{route('product/List')}}">Product List</a>
+
+
+
+
+
